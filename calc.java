5f8b0a03c99554e65338234c0bbac4d285364152
Justin Zhang <justinzhang20711@gmail.com>
2023-05-23 08:59:05 -0700

    some javadoc

--- calc.java
+++ calc.java
@@ -1,19 +1,52 @@
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.ArrayList;
+import java.util.Scanner; // Import the Scanner class to read text files
+
 
 
 public class calc {
+
+
     private final double TIME_BETWEEN_FRAMES = 1/240;
-    
     //outputs to coords.txt
     //f, [[x, y, s], [dx, dy, ds]]
+    //What I need from Monesh
+    //pixelHeight of the ball from the ground at initial pos and 2nd frame pos
+    //need width and pixelWidth of the original ball at distanceFixed away
+    //need pixelWidth1 and pixelWidth2 of the ball in the two different frames
+    
 
+    private graphable[] graphablePoints;
+    
     public calc (){
 
         //access file and stuff
+        
+        
+    }
 
+    private ArrayList<String> accessFile() {
+        ArrayList<String> strArray = new ArrayList<String>();
+        
+        try {
+            File myObj = new File("coord.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+
+                strArray.add(data);
+            }
+            myReader.close();
+        } 
+        catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+        return strArray;
     }
     
-    private double x1, y1; 
-    private double x2, y2;
 
 
     //creating an object, constructor looks at text file. 
