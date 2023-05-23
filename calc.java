6d798c9293aacf3d1f4e486b97f1cdd565e1c18e
Justin Zhang <justinzhang20711@gmail.com>
2023-05-23 08:36:37 -0700

    calc framework

--- calc.java
+++ calc.java
@@ -3,15 +3,25 @@
 public class calc {
     private final double TIME_BETWEEN_FRAMES = 1/240;
     
+    //outputs to coords.txt
     //f, [[x, y, s], [dx, dy, ds]]
 
+    public calc (){
+
+        //access file and stuff
+
+    }
     
     private double x1, y1; 
     private double x2, y2;
 
 
     //creating an object, constructor looks at text file. 
-    //then uses calc.xEquation(_____) to claculate stuff.
+
+    //then uses calc.getEquations(_____) that outputs an array of type graphable
+
+    //just wanna let u know that the calc.getEquations(y) is an angle and not a set of points
+
 }
 
 
